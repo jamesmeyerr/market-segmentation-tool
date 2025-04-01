9fcb82b6aeabe305170ec8d01fd61b9f526103cc
James Meyer <jamesmeyerr77@gmail.com>
2025-04-01 12:47:02 +1100

    remove pandas from app.py

--- backend/api/app.py
+++ backend/api/app.py
@@ -2,7 +2,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS
 import os
-import pandas as pd
 from ..data_processor import DataProcessor
 from ..cluster_engine import ClusterEngine
 
